bffd8d95425b91935349de17a923a2a04e1281be
Bruno Bartolomeu <42076192+Bmartins25@users.noreply.github.com>
2023-10-14 18:18:13 -0300

    Add files via upload

---
+++ ModeloBrunoML.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# import libraries
+import boto3, re, sys, math, json, os, sagemaker, urllib.request
+from sagemaker import get_execution_role
+import numpy as np                                
+import pandas as pd                               
+import matplotlib.pyplot as plt                   
+from IPython.display import Image                 
+from IPython.display import display               
+from time import gmtime, strftime                 
+from sagemaker.predictor import csv_serializer   
+
+# Define IAM role
+role = get_execution_role()
+prefix = 'sagemaker/DEMO-xgboost-dm'
+containers = {'us-west-2': '433757028032.dkr.ecr.us-west-2.amazonaws.com/xgboost:latest',
+              'us-east-1': '811284229777.dkr.ecr.us-east-1.amazonaws.com/xgboost:latest',
+              'us-east-2': '825641698319.dkr.ecr.us-east-2.amazonaws.com/xgboost:latest',
+              'eu-west-1': '685385470294.dkr.ecr.eu-west-1.amazonaws.com/xgboost:latest'} # each region has its XGBoost container
+my_region = boto3.session.Session().region_name # set the region of the instance
+print("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + containers[my_region] + " container for your SageMaker endpoint.")
+
+
+# In[ ]:
+
+
+bucket_name = 'my.modelobruno.s3.bucket' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
+s3 = boto3.resource('s3')
+try:
+    if  my_region == 'us-east-1':
+      s3.create_bucket(Bucket=bucket_name)
+    else: 
+      s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={ 'LocationConstraint': my_region })
+    print('S3 bucket created successfully')
+except Exception as e:
+    print('S3 error: ',e)
+
+
+# In[3]:
+
+
+try:
+  urllib.request.urlretrieve ("https://d1.awsstatic.com/tmt/build-train-deploy-machine-learning-model-sagemaker/bank_clean.27f01fbbdf43271788427f3682996ae29ceca05d.csv", "bank_clean.csv")
+  print('Success: downloaded bank_clean.csv.')
+except Exception as e:
+  print('Data load error: ',e)
+
+try:
+  model_data = pd.read_csv('./bank_clean.csv',index_col=0)
+  print('Success: Data loaded into dataframe.')
+except Exception as e:
+    print('Data load error: ',e)
+
+
+# In[4]:
+
+
+train_data, test_data = np.split(model_data.sample(frac=1, random_state=1729), [int(0.7 * len(model_data))])
+print(train_data.shape, test_data.shape)
+
+
+# In[5]:
+
+
+df = pd.read_csv('bank_clean.csv')
+df.head()
+
+
+# In[6]:
+
+
+import sys
+get_ipython().system('{sys.executable} -m pip install xgboost')
+
+
+# In[7]:
+
+
+df = pd.read_csv('train.csv')
+df.head()
+
+
+# In[8]:
+
+
+pd.concat([train_data['y_yes'], train_data.drop(['y_no', 'y_yes'], axis=1)], axis=1).to_csv('train.csv', index=False, header=False)
+boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/train.csv')).upload_file('train.csv')
+s3_input_train = sagemaker.TrainingInput(s3_data='s3://{}/{}/train'.format(bucket_name, prefix), content_type='csv')
+
+
+# In[9]:
+
+
+xgb.fit({'train': s3_input_train})
+
+
+# In[ ]:
+
+
+xgb_predictor = xgb.deploy(initial_instance_count=1,instance_type='ml.m4.xlarge')
+
+
+# In[ ]:
+
+
+test_data_array = test_data.drop(['y_no', 'y_yes'], axis=1).values #load the data into an array
+xgb_predictor.serializer = csv_serializer # set the serializer type
+predictions = xgb_predictor.predict(test_data_array).decode('utf-8') # predict!
+predictions_array = np.fromstring(predictions[1:], sep=',') # and turn the prediction into an array
+print(predictions_array.shape)
+
+
+# In[ ]:
+
+
+cm = pd.crosstab(index=test_data['y_yes'], columns=np.round(predictions_array), rownames=['Observed'], colnames=['Predicted'])
+tn = cm.iloc[0,0]; fn = cm.iloc[1,0]; tp = cm.iloc[1,1]; fp = cm.iloc[0,1]; p = (tp+tn)/(tp+tn+fp+fn)*100
+print("\n{0:<20}{1:<4.1f}%\n".format("Overall Classification Rate: ", p))
+print("{0:<15}{1:<15}{2:>8}".format("Predicted", "No Purchase", "Purchase"))
+print("Observed")
+print("{0:<15}{1:<2.0f}% ({2:<}){3:>6.0f}% ({4:<})".format("No Purchase", tn/(tn+fn)*100,tn, fp/(tp+fp)*100, fp))
+print("{0:<16}{1:<1.0f}% ({2:<}){3:>7.0f}% ({4:<}) \n".format("Purchase", fn/(tn+fn)*100,fn, tp/(tp+fp)*100, tp))
+
+
+# In[ ]:
+
+
+sagemaker.Session().delete_endpoint(xgb_predictor.endpoint)
+bucket_to_delete = boto3.resource('s3').Bucket(bucket_name)
+bucket_to_delete.objects.all().delete()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
